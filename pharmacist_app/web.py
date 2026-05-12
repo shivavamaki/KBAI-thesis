f665710e9a1489b61d4b91209bfaaea732770e87
KBAI Thesis <shivavamaki@gmail.com>
2026-05-12 10:21:35 +0700

    change

--- pharmacist_app/web.py
+++ pharmacist_app/web.py
@@ -656,6 +656,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "text/html; charset=utf-8")
         self.send_header("Content-Length", str(len(body)))
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
         self.end_headers()
         self.wfile.write(body)
 
